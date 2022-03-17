122581cc15053cb12d79369ada169e526edf46b2
Erick Guzman <guzmi.dev@gmail.com>
2022-03-16 17:05:37 -0700

    :art: routes

--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,22 @@
 import express, { Application } from "express";
+import userRoutes from "../routes/usuario.route";
 
 class Server {
   private app: Application;
   private port: string;
+  private apiPaths = {
+    usuarios: "/api/usuarios",
+  };
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT || "8000";
+
+    this.routes();
+  }
+
+  routes() {
+    this.app.use(this.apiPaths.usuarios, userRoutes);
   }
 
   listen() {
